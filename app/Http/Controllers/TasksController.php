0ccfc827fab087069c85a8f928061708dc8d168e
hiro <hiroshima6504@gmail.com>
2017-09-10 13:22:59 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,12 +48,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'title' => 'required|max:255',   // 追加
+            'status' => 'required',   // 追加
             'content' => 'required|max:255',
         ]);
         
         $task = new Task;
-        $task->title = $request->title;    // 追加
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
     }
@@ -98,12 +98,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'title' => 'required|max:255',   // 追加
+            'status' => 'required',   // 追加
             'content' => 'required|max:255',
         ]);
         
         $task = Task::find($id);
-        $task->title = $request->title;    // 追加
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
