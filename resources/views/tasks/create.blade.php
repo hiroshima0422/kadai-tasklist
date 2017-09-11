9816934ae338f64aff8ae160c942ade4ec7b98dc
hiro <hiroshima6504@gmail.com>
2017-09-11 13:43:25 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,13 +6,22 @@
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
+    <div class="form-group">
+        <div class="row">
+            <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('status', 'ステータス:') !!}</div>
+            <div class="col-sm-4 col-lg-3">{!! Form::text('status', null, ['class' => 'form-control']) !!}</div>
+        </div>
+    </div>
+    
+    <div class="form-group">
+        <div class="row">
+            <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('content', 'メッセージ:') !!}</div>
+            <div class="col-sm-4 col-lg-3">{!! Form::text('content', null, ['class' => 'form-control']) !!}</div>
+        </div>
+    </div>
+    <div class="row">
+            <div class="col-sm-offset-2 col-lg-offset-3">{!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}</div>
+    </div>
 
     {!! Form::close() !!}
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,14 +5,20 @@
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('status', 'ステータス:') !!}</div>
+                <div class="col-sm-4 col-lg-3">{!! Form::text('status', null, ['class' => 'form-control']) !!}</div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('content', 'メッセージ:') !!}</div>
+                <div class="col-sm-4 col-lg-3">{!! Form::text('content', null, ['class' => 'form-control']) !!}</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-offset-2 col-lg-offset-3">{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}</div>
+        </div>
     {!! Form::close() !!}
 @endsection
