d32b7a33f2b84c9a523ac628eeb0c9322e668d38
hiro <hiroshima6504@gmail.com>
2017-09-12 15:54:31 +0000

    new-bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,26 +3,21 @@
 @section('content')
 
     <h1>メッセージ新規作成ページ</h1>
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-    <div class="form-group">
-        <div class="row">
-            <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('status', 'ステータス:') !!}</div>
-            <div class="col-sm-4 col-lg-3">{!! Form::text('status', null, ['class' => 'form-control']) !!}</div>
-        </div>
-    </div>
-    
-    <div class="form-group">
-        <div class="row">
-            <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('content', 'メッセージ:') !!}</div>
-            <div class="col-sm-4 col-lg-3">{!! Form::text('content', null, ['class' => 'form-control']) !!}</div>
-        </div>
-    </div>
     <div class="row">
-            <div class="col-sm-offset-2 col-lg-offset-3">{!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}</div>
+        <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        
+                <div class="form-group">
+                        <div>{!! Form::label('status', 'ステータス:') !!}</div>
+                        <div>{!! Form::text('status', null, ['class' => 'form-control']) !!}</div>
+                </div>
+                <div class="form-group">
+                    <div>{!! Form::label('content', 'メッセージ:') !!}</div>
+                    <div>{!! Form::text('content', null, ['class' => 'form-control']) !!}</div>
+                </div>
+                <div>{!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}</div>
+            
+            {!! Form::close() !!}
+        </div>    
     </div>
-
-    {!! Form::close() !!}
-
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,22 +3,19 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
-
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-        <div class="form-group">
-            <div class="row">
-                <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('status', 'ステータス:') !!}</div>
-                <div class="col-sm-4 col-lg-3">{!! Form::text('status', null, ['class' => 'form-control']) !!}</div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">{!! Form::label('content', 'メッセージ:') !!}</div>
-                <div class="col-sm-4 col-lg-3">{!! Form::text('content', null, ['class' => 'form-control']) !!}</div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-offset-2 col-lg-offset-3">{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}</div>
-        </div>
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-sm-offset-2 col-sm-4 col-lg-offset-3 col-lg-3">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                        <div>{!! Form::label('status', 'ステータス:') !!}</div>
+                        <div>{!! Form::text('status', null, ['class' => 'form-control']) !!}</div>
+                </div>
+                <div class="form-group">
+                        <div>{!! Form::label('content', 'メッセージ:') !!}</div>
+                        <div>{!! Form::text('content', null, ['class' => 'form-control']) !!}</div>
+                </div>
+                git remote add origin https://github.com/hiroshima0422/kadai-tasklist-Bootstrap.git<div>{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}</div>
+            {!! Form::close() !!}
+        </div>    
+    </div>
 @endsection
