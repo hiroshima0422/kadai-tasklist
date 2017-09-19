dfc1c5c77aefe7530d9fafe1eb806586acd240a4
hiro <hiroshima6504@gmail.com>
2017-09-19 15:43:12 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
 
 //Route::get('/', 'TasksController@index');
 
